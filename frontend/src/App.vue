ad70509502393aa062c744b78d82d6f1a1f3c923
wuyupeng <egaQK1QY&#*0aaGU>
2025-09-20 18:27:17 +0800

    feat: 页面布局修改

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,26 @@
 <script setup></script>
 
 <template>
-  <RouterView />
+  <el-container class="app-container">
+    <el-container class="main-container">
+      <el-header>
+        <RouterLink to="/">首页</RouterLink>
+        <RouterLink to="/weather-forecast">天气预测</RouterLink>
+      </el-header>
+      <el-container>
+        <RouterView />
+      </el-container>
+    </el-container>
+    <el-footer>
+      页脚信息 SeedSearcher © 2024 Created
+    </el-footer>
+  </el-container>
 </template>
 
 <style scoped lang="scss">
+.app-container {
+  .main-container {
+    height: 100vh;
+  }
+}
 </style>
